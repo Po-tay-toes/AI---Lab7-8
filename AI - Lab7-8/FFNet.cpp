5f0890310f774483f56ac7bb438994e4d576693a
Jack Burton <s5076332@bournemouth.ac.uk>
2018-10-30 17:45:23 +0000

    FFNet finished
    Finished the FFNet using a step function.
    --Things to do--
    Change the step function to a sigmoid function.

--- AI - Lab7-8/FFNet.cpp
+++ AI - Lab7-8/FFNet.cpp
@@ -5,9 +5,10 @@ FFNet::FFNet()
 	alpha = 0.1f;
 	theta = 0.2f;
 	delta = 0.000001f;
-	w1 = 0.1f , w2 = 0.1f , w3 = 0.1f, w4 = 0.1f, w5 = 0.2f, w6 = 0.2f;
+	w1 = 0.5f , w2 = 0.9f , w3 = 0.4f, w4 = 1.0f, w5 = -1.2f, w6 = 1.1f;
 
-	for (int p = 0; p < 20; p++)
+
+	for (int p = 0; p < 64; p++)
 	{
 		int i = p % 4;
 		int o = abs(i - 3);
@@ -19,56 +20,56 @@ FFNet::FFNet()
 
 		if (X >= theta)
 		{
-			h1[i] = 1;
-			std::cout << "h1 = " << h1[i] << std::endl; 
+			h1 = 1;
+			std::cout << "h1 = " << h1 << std::endl; 
 		}
 		else 
 		{
-			h1[i] = 0;
-			std::cout << "h1 = " << h1[i] << std::endl;
+			h1 = 0;
+			std::cout << "h1 = " << h1 << std::endl;
 		}
+
+		e = h1d[i] - h1;
+		w1 = w1 + alpha * x1[i] * e;
+		std::cout << "New w1 = " << w1 << std::endl;
+		w2 = w2 + alpha * x2[o] * e;
+		std::cout << "New w2 = " << w2 << std::endl;
 
 		if (X2 >= theta)
 		{
-			h2[i] = 1;
-			std::cout << "h2 = " << h2[i] << std::endl;
+			h2 = 1;
+			std::cout << "h2 = " << h2 << std::endl;
 		}
 		else
 		{
-			h2[i] = 0;
-			std::cout << "h2 = " << h2[i] << std::endl;
+			h2 = 0;
+			std::cout << "h2 = " << h2 << std::endl;
 		}
 
-		X3 = (h1[i] * w5) + (h2[i] * w6);
+		e2 = h2d[i] - h2;
+		w3 = w3 + alpha * x1[o] * e2;
+		std::cout << "New w3 = " << w3 << std::endl;
+		w4 = w4 + alpha * x2[i] * e2;
+		std::cout << "New w4 = " << w4 << std::endl;
+
+		X3 = (h1 * w5) + (h2 * w6);
 
 		if (X3 >= theta)
 		{
 			y = 1;
-			std::cout << "y = " << y  << "\n" << std::endl;
+			std::cout << "y = " << y  << std::endl;
 		}
 		else
 		{
 			y = 0;
-			std::cout << "y = " << y << "\n" << std::endl;
+			std::cout << "y = " << y << std::endl;
 		}
 
-		/*
-		e = Yd[i] - y;
-		std::cout << "e = " << e << std::endl;
-
-		w1 = w1 + alpha * x1[i] * e;
-		std::cout << "new w1 = " << w1 << std::endl;
-		w2 = w2 + alpha * x1[i] * e;
-		std::cout << "new w2 = " << w2 << std::endl;
-		w3 = w3 + alpha * x1[i] * e;
-		std::cout << "new w3 = " << w3 << std::endl;
-		w4 = w4 + alpha * x1[i] * e;
-		std::cout << "new w4 = " << w4 << std::endl;
-		w5 = w5 + alpha * x1[i] * e;
-		std::cout << "new w5 = " << w5 << std::endl;
-		w6 = w6 + alpha * x1[i] * e;
-		std::cout << "new w6 = " << w6 << "\n" << std::endl;
-		*/
+		e3 = Yd[i] - y;
+		w5 = w5 + alpha * h1 * e3;
+		std::cout << "New w5 = " << w5 << std::endl;
+		w6 = w6 + alpha * h2 * e3;
+		std::cout << "New w6 = " << w6 << "\n" << std::endl;
 
 	}
 }
--- AI - Lab7-8/FFNet.h
+++ AI - Lab7-8/FFNet.h
@@ -5,13 +5,13 @@ class FFNet
 {
 private:
 
-	float y, X, X2, X3, theta, delta, alpha, e, w1, w2, w3, w4, w5, w6;
+	float y, X, X2, X3, theta, delta, alpha, e, e2, e3, w1, w2, w3, w4, w5, w6, h1, h2;
 
 	float x1[4] = { 0, 0, 1, 1 };
 	float x2[4] = { 0, 1, 0, 1 };
-	float h1[4] = { 0 };
-	float h2[4] = { 0 };
 	float Yd[4] = { 0, 1, 1, 0 };
+	float h1d[4] = { 0, 0, 1, 0 };
+	float h2d[4] = { 0, 1, 0, 0 };
 
 public:
 	FFNet();
